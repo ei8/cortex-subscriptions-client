0a7535e0295fd17464f4f0dd7ae964568c4d813c
Junvic <contact@junvic.me>
2022-08-30 02:49:12 +0800

    Support for generic subscriptions (#3)
    * refactor: separate subscription info from receiver info
    
    * refactor: make parsing attribute an extension method

--- src/main/In/HttpSubscriptionsClient.cs
+++ src/main/In/HttpSubscriptionsClient.cs
@@ -1,4 +1,6 @@
 ﻿using ei8.Cortex.Subscriptions.Common;
+using ei8.Cortex.Subscriptions.Common.Extensions;
+using ei8.Cortex.Subscriptions.Common.Receivers;
 using neurUL.Common.Http;
 using NLog;
 using Polly;
@@ -10,7 +12,7 @@ using System.Threading.Tasks;
 
 namespace ei8.Cortex.Subscriptions.Client.In
 {
-    public class HttpSubscriptionsClient : ISubscriptionsClient
+    public class HttpSubscriptionsClient<T> : ISubscriptionsClient<T> where T : IReceiverInfo
     {
         private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
 
@@ -18,7 +20,7 @@ namespace ei8.Cortex.Subscriptions.Client.In
                                                                  .WaitAndRetryAsync(
                                                                     3,
                                                                     attempt => TimeSpan.FromMilliseconds(100 * Math.Pow(2, attempt)),
-                                                                    (ex, _) => HttpSubscriptionsClient.Logger.Error(ex, "Error occurred while communicating with ei8 Cortex Subscriptions. " + ex.InnerException?.Message)
+                                                                    (ex, _) => HttpSubscriptionsClient<T>.Logger.Error(ex, "Error occurred while communicating with ei8 Cortex Subscriptions. " + ex.InnerException?.Message)
                                                                  );
         private IRequestProvider requestProvider;
 
@@ -27,14 +29,15 @@ namespace ei8.Cortex.Subscriptions.Client.In
             this.requestProvider = requestProvider ?? Locator.Current.GetService<IRequestProvider>();
         }
 
-        public async Task AddSubscription(string baseUrl, BrowserSubscriptionInfo request, CancellationToken token = default)
+        public async Task AddSubscription(string baseUrl, IAddSubscriptionReceiverRequest<T> request, CancellationToken token = default)
         {
             await ExponentialRetry.ExecuteAsync(async () => await AddSubscriptionInternal(baseUrl, request, token).ConfigureAwait(false));
         }
 
-        private async Task AddSubscriptionInternal(string baseUrl, BrowserSubscriptionInfo request, CancellationToken token = default)
+        private async Task AddSubscriptionInternal(string baseUrl, IAddSubscriptionReceiverRequest<T> request, CancellationToken token = default)
         {
-            var requestUrl = $"{baseUrl}/subscriptions";
+            var subscriptionPath = request.ReceiverInfo.GetSubscriptionPath();
+            var requestUrl = $"{baseUrl}/subscriptions/receivers/{subscriptionPath}";
 
             await requestProvider.PostAsync(requestUrl, request);
         }
