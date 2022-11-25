3ed3ffbadbc11e958a9aa0a6c4f19c25fe4d2836
Junvic <contact@junvic.me>
2022-11-25 16:24:44 +0800

    Support for access request handler (#4)
    * feature: add send notification method to client
    
    * Update Client.csproj
    
    Co-authored-by: elmerbool <35117496+elmerbool@users.noreply.github.com>

--- src/main/In/ISubscriptionsClient.cs
+++ src/main/In/ISubscriptionsClient.cs
@@ -8,5 +8,7 @@ namespace ei8.Cortex.Subscriptions.Client.In
     public interface ISubscriptionsClient 
     {
         Task AddSubscription<T>(string baseUrl, IAddSubscriptionReceiverRequest<T> request, CancellationToken token = default) where T : IReceiverInfo;
+
+        Task SendNotificationToUser(string baseUrl, string targetUserNeuronId, NotificationPayloadRequest request, CancellationToken token = default);
     }
 }
